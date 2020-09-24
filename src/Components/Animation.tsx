35314f065bafeb1fd9b85fabb934997fce3311ea
Troels Lund <trolund@gmail.com>
2020-09-24 20:02:51 +0200

    more settings

--- src/Components/Animation.tsx
+++ src/Components/Animation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Sketch from "react-p5";
 import p5Types from "p5"; //Import this for typechecking and intellisense
 import { Point } from "../Models/Point";
@@ -21,18 +21,34 @@ const P5: React.FC<ComponentProps> = (props: ComponentProps) => {
   const [sizeMin, setSizeMin] = useState(10);
   const [margin, setMargin] = useState(100);
 
+  const [canvasWidth, setCanvasWidth] = useState(100);
+  const [canvasHeight, setCanvasHeight] = useState(100);
+
+  const [connections, setConnections] = useState(2);
+
   //let clusterSize = 5;
   // let clusterNumbers = 1;
-  let connections = 2;
+  // let connections = 2;
   // let margin = 100;
-  let canvasWidth = 800;
-  let canvasHeight = 500;
+  // let canvasWidth = 800;
+  // let canvasHeight = 500;
   //let smoothAnimation = true;
   // let maxSpeed = 3;
   //let minSpeed = 0;
   // let sizeMax = 20;
   // let sizeMin = 10;
   let bgColor: number = 10;
+
+  const resizeHandler = useCallback(() => {
+    setCanvasWidth((window as any).innerWidth * 0.85);
+    setCanvasHeight((window as any).innerHeight);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("resize", resizeHandler);
+    resizeHandler();
+    return () => document.removeEventListener("resize", resizeHandler);
+  }, [resizeHandler]);
 
   const resetClickHandler = () => {
     setPlaying(false);
@@ -117,8 +133,16 @@ const P5: React.FC<ComponentProps> = (props: ComponentProps) => {
     // console.log("setup!");
   };
 
+  const reducer = (accumulator: number, currentValue: Point[][]) =>
+    accumulator + currentValue.length;
+
   const draw = (p5: p5Types) => {
-    if (playing && custers !== null) {
+    if (
+      playing &&
+      custers !== null &&
+      custers.length === clusterNumbers &&
+      custers[0].length === clusterSize
+    ) {
       p5.background(bgColor);
       for (var c = 0; c < clusterNumbers; c++) {
         for (var i = 0; i < clusterSize; i++) {
@@ -131,56 +155,88 @@ const P5: React.FC<ComponentProps> = (props: ComponentProps) => {
   };
 
   return (
-    <>
-      <button onClick={resetClickHandler}>Reset</button>
-      <button onClick={SmoothAniClickHandler}>
-        Smooth animation : {smoothAnimation ? "ON" : "OFF"}
-      </button>
-      <div className="slidecontainer">
-        <p>Max movment speed of nodes : {maxSpeed}</p>
-        <input
-          type="range"
-          min={minSpeed}
-          max="50"
-          value={maxSpeed}
-          onChange={(e) => setMaxSpeed(Number(e.target.value))}
-        />
-        <p>Min movment speed of nodes : {minSpeed}</p>
-        <input
-          type="range"
-          min="0"
-          max={maxSpeed}
-          value={minSpeed}
-          onChange={(e) => setMinSpeed(Number(e.target.value))}
-        />
+    <div className="container">
+      <div className="options">
+        <button onClick={resetClickHandler}>Reset</button>
+        <button onClick={SmoothAniClickHandler}>
+          Smooth animation : {smoothAnimation ? "ON" : "OFF"}
+        </button>
+        <div className="slidecontainer">
+          <p>Max movment speed of nodes : {maxSpeed}</p>
+          <input
+            type="range"
+            min={minSpeed}
+            max="50"
+            value={maxSpeed}
+            onChange={(e) => setMaxSpeed(Number(e.target.value))}
+          />
+          <p>Min movment speed of nodes : {minSpeed}</p>
+          <input
+            type="range"
+            min="0"
+            max={maxSpeed}
+            value={minSpeed}
+            onChange={(e) => setMinSpeed(Number(e.target.value))}
+          />
 
-        <p>Max size : {sizeMax}</p>
-        <input
-          type="range"
-          min={sizeMin}
-          max="50"
-          value={sizeMax}
-          onChange={(e) => setSizeMax(Number(e.target.value))}
-        />
-        <p>Min size : {sizeMin}</p>
-        <input
-          type="range"
-          min="0"
-          max={sizeMax}
-          value={sizeMin}
-          onChange={(e) => setSizeMin(Number(e.target.value))}
-        />
-        <p>Margin : {margin}</p>
-        <input
-          type="range"
-          min="0"
-          max="200"
-          value={margin}
-          onChange={(e) => setMargin(Number(e.target.value))}
-        />
+          <p>Max node size : {sizeMax}</p>
+          <input
+            type="range"
+            min={sizeMin}
+            max="50"
+            value={sizeMax}
+            onChange={(e) => setSizeMax(Number(e.target.value))}
+          />
+          <p>Min node size : {sizeMin}</p>
+          <input
+            type="range"
+            min="0"
+            max={sizeMax}
+            value={sizeMin}
+            onChange={(e) => setSizeMin(Number(e.target.value))}
+          />
+          <p>Margin : {margin}</p>
+          <input
+            type="range"
+            min="0"
+            max="200"
+            value={margin}
+            onChange={(e) => setMargin(Number(e.target.value))}
+          />
+          <p>Connections per Node: {connections}</p>
+          <input
+            type="range"
+            min="0"
+            max={clusterSize - 1}
+            value={connections}
+            onChange={(e) => setConnections(Number(e.target.value))}
+          />
+        </div>
+        <div>
+          <p>Nodes in group: {clusterNumbers}</p>
+          <input
+            type="range"
+            min="1"
+            max="40"
+            value={clusterNumbers}
+            onChange={(e) => clusterNumberChangeHandler(Number(e.target.value))}
+          />
+          <p>Cluster size: {clusterSize}</p>
+          <input
+            type="range"
+            min="3"
+            max="40"
+            value={clusterSize}
+            onChange={(e) => setClusterSize(Number(e.target.value))}
+          />
+        </div>
+        <p>number of clusters : {custers?.length}</p>
+        <p>number of nodes in cluster {}</p>
       </div>
-      <Sketch setup={setup} draw={draw} />
-    </>
+      <div className="canvas">
+        <Sketch setup={setup} draw={draw} />
+      </div>
+    </div>
   );
 };
 
